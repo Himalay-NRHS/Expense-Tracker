1f2a3630b4efc86037db4923f1a2c5487d4cd78b
Himalay <himalayrekha@gmail.com>
2025-02-16 01:01:15 +0530

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import type React from "react"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { DollarSignIcon, HistoryIcon, PieChartIcon, PlusIcon } from "lucide-react"
-
+import a from "../lib/db"
 export default function Home() {
   return (
     <div className="flex-1">
@@ -11,7 +11,7 @@ export default function Home() {
         <div className="flex flex-col items-center space-y-4 text-center">
           <div className="space-y-2">
             <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-              Manage Your Finances with Ease  NTG
+              Manage Your Finances with Ease  {a}
             </h1>
             <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
               Track your expenses, monitor your income, and take control of your financial future with our powerful
--- lib/db.ts
+++ lib/db.ts
@@ -1,2 +1,38 @@
-import {Client } from "pg"
-const pgclient=new Client(process.env.DATABASE_URL)
+import { Client } from "pg";
+const pgclient = new Client({
+    user: "postgres",
+    host: "localhost",
+    database: "expense-tracker",
+    password: "himu",
+    port: 5432,
+  });
+const query1 = `CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    amount INT DEFAULT 0
+);
+`;
+const query2=`CREATE TABLE IF NOT EXISTS transactions (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    amount INT NOT NULL,
+    transaction_type VARCHAR(50) NOT NULL,  -- e.g., 'expense' or 'credit'
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+`
+
+async function main() {
+  try {
+    await pgclient.connect();
+    console.log("Connected to DB");
+    await pgclient.query(query1);
+    await pgclient.query(query2)
+  } catch (err) {
+    console.error("Database Error:", err);
+  }
+}
+main()
+export default pgclient;
