826a268fb55beefca16e383b2c6a5d664eb27aef
Himalay <himalayrekha@gmail.com>
2025-03-16 12:19:31 +0530

    migrations

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,12 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 //import pgclient from "@/lib/db";
 import prisma from "@/lib/db";
 import bcrypt from "bcryptjs";
+if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
+  throw new Error('Missing Google client ID or secret');
+}
 
 const handler = NextAuth({
   providers: [
@@ -24,7 +28,7 @@ const handler = NextAuth({
         if(!user){
           throw new Error("Invalid email or password");
         }
-        const passwordMatch = await bcrypt.compare(credentials.password, user.password);
+        const passwordMatch = await bcrypt.compare(credentials.password, user.password!);
 
         if (!passwordMatch) {
           throw new Error("Invalid credentials");
@@ -34,7 +38,47 @@ const handler = NextAuth({
 
         
       }
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
+
     })
-  ]
-  })
+  ], callbacks: {
+    async signIn({ account, profile }) {
+      if (account?.provider === "google") {
+        // Check if user exists in DB, else create new user
+        const existingUser = await prisma.users.findUnique({
+          where: { email: profile?.email },
+        });
+
+        if (!existingUser) {
+          await prisma.users.create({
+            data: {
+              email: profile?.email!,
+              name: profile?.name!,
+              password: "NULL",
+            },
+          });
+        }
+      }
+      return true;
+    },
+    async session({ session, user, token }) {
+      if (session.user && token.email) {
+        session.user.email = token.email;
+      }
+      return session;
+    },
+  },
+});
+
+ 
   export { handler as GET, handler as POST }
